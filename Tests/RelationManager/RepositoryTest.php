9bde050b8dcd1b25ba36b25baabf746fc97f2d0f
DavidBadura <d.badura@gmx.de>
2012-01-21 11:33:23 +0100

    fix repository and add tests

--- Tests/RelationManager/RepositoryTest.php
+++ Tests/RelationManager/RepositoryTest.php
@@ -15,9 +15,23 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
 
         $repo = new Repository();
         $this->assertCount(0, $repo);
+        $this->assertFalse($repo->has('any_object'));
 
-        $object = new stdClass();
+        $object1 = new  \stdClass();
 
+        $repo->set('any_object', $object1);
+        $this->assertCount(1, $repo);
+        $this->assertTrue($repo->has('any_object'));
+        $this->assertEquals($object1, $repo->get('any_object'));
+
+        $object2 = new  \stdClass();
+
+        $repo->set('other_object', $object2);
+        $this->assertCount(2, $repo);
+        $this->assertTrue($repo->has('any_object'));
+        $this->assertTrue($repo->has('other_object'));
+        $this->assertEquals($object1, $repo->get('any_object'));
+        $this->assertEquals($object2, $repo->get('other_object'));
 
     }
 
