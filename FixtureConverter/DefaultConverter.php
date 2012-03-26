67c31a274b6aa915a3b85d4c08334eb428bc8b11
DavidBadura <d.badura@gmx.de>
2012-03-26 15:49:20 +0200

    update DefaultConverter

--- FixtureConverter/DefaultConverter.php
+++ FixtureConverter/DefaultConverter.php
@@ -18,9 +18,23 @@ class DefaultConverter extends FixtureConverter
         $data = $fixtureData->getData();
         $class = $properties['class'];
 
-        $object = new $class();
-        foreach ($data as $key => $value) {
-            $this->setVar($object, $key, $value);
+        $constructor = $properties['constructor'];
+
+        $object = null;
+        if(empty($constructor)) {
+            $object = new $class();
+        } else {
+            $args = array();
+            foreach($constructor as $arg) {
+                $args[] = $data[$arg];
+            }
+
+            $reflection = new ReflectionClass($class);
+            $object = $reflection->newInstanceArgs($args);
+        }
+
+        foreach ($data as $property => $value) {
+            $this->writeProperty($object, $property, $value);
         }
 
         return $object;
@@ -40,9 +54,38 @@ class DefaultConverter extends FixtureConverter
         return $properties;
     }
 
-    private function setVar($object, $key, $value)
+    /**
+     * Sets the value of the property at the given index in the path
+     *
+     * @param object  $objectOrArray The object or array to traverse
+     * @param integer $currentIndex  The index of the modified property in the path
+     * @param mixed $value           The value to set
+     */
+    protected function writeProperty($object, $property, $value)
     {
-        $object->$key = $value;
-    }
+        $reflClass = new \ReflectionClass($object);
+        $setter = 'set'.$this->camelize($property);
 
+        if ($reflClass->hasMethod($setter)) {
+            if (!$reflClass->getMethod($setter)->isPublic()) {
+                throw new PropertyAccessDeniedException(sprintf('Method "%s()" is not public in class "%s"', $setter, $reflClass->getName()));
+            }
+
+            $object->$setter($value);
+        } elseif ($reflClass->hasMethod('__set')) {
+            // needed to support magic method __set
+            $objectOrArray->$property = $value;
+        } elseif ($reflClass->hasProperty($property)) {
+            if (!$reflClass->getProperty($property)->isPublic()) {
+                throw new PropertyAccessDeniedException(sprintf('Property "%s" is not public in class "%s". Maybe you should create the method "%s()"?', $property, $reflClass->getName(), $setter));
+            }
+
+            $objectOrArray->$property = $value;
+        } elseif (property_exists($object, $property)) {
+            // needed to support \stdClass instances
+            $objectOrArray->$property = $value;
+        } else {
+            throw new InvalidPropertyException(sprintf('Neither element "%s" nor method "%s()" exists in class "%s"', $property, $setter, $reflClass->getName()));
+        }
+    }
 }
